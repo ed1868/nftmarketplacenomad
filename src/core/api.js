5dae2a4e18dd382e0f79180008caa7049901a956
ed1868 <ruizeduardo21@gmail.com>
2021-11-13 16:49:13 -0500

    template code

---
+++ src/core/api.js
@@ -0,0 +1,11 @@
+const api = {
+    baseUrl: 'http://localhost:1337',
+    nfts: '/nfts',
+    nftShowcases: '/nft_showcases',
+    authors: '/authors',
+    authorsSales: '/author_ranks',
+    hotCollections: '/hot-collections',
+    contactUs: '/contact-forms'
+}
+
+export default api;
